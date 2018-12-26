b12c55d0a934820508eef9a88df193ee732b9a80
logicboy <faithfulanere@gmail.com>
2018-12-26 22:40:56 +0100

    Revert "Types and Data"
    This reverts commit ece8edc00b6c6a419a0ed2ea3bf0c0b7cc02feb1.

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const graphqlHttp = require('express-graphql')
-const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.use(
-  '/graphql',
-  graphqlHttp({
-    schema: buildSchema(
-      `
-        type RootQuery {
-            events: [String!]!
-        }
-        type RootMutation {
-            createEvent(name: String): String
-        }
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `
-    ),
-    rootValue: {
-      events: () => {
-        return [ 'Romantic Cooking Event', 'Sailing', 'Soccer' ]
-      },
-      createEvent: args => {
-        const eventName = args.name
-        return eventName
-      }
-    },
-    graphiql: true
-  })
-)
+app.get('/', (req, res, next) => {
+  res.send('Holla Fellas, Server works')
+})
 
 app.listen(3000)
