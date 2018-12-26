8d105e4f8433e44554cb52b080ade9f44aad854f
logicboy <faithfulanere@gmail.com>
2018-12-26 22:41:36 +0100

    Schemas and Resolvers

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send('Holla Fellas, Server works')
-})
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(
+      `
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `
+    ),
+    rootValue: {
+      events: () => {
+        return [ 'Romantic Cooking Event', 'Sailing', 'Soccer' ]
+      },
+      createEvent: args => {
+        const eventName = args.name
+        return eventName
+      }
+    },
+    graphiql: true
+  })
+)
 
 app.listen(3000)
